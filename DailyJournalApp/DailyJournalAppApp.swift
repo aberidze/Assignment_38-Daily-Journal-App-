a2382f164a2b69d5eafc46c0d606bdb3021e3a48
AnriBeridzee <aberidze02@gmail.com>
2023-12-21 03:54:04 +0400

    Initial Commit

---
+++ DailyJournalApp/DailyJournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyJournalAppApp.swift
+//  DailyJournalApp
+//
+//  Created by Macbook Air 13 on 21.12.23.
+//
+
+import SwiftUI
+
+@main
+struct DailyJournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
